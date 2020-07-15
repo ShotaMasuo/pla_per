b694c9a5f03445ca2d91b2acc5801fadcf5c1756
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-07-15 17:44:38 +0900

    create-frontend

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,8 @@
 class SchedulesController < ApplicationController
   def index
-
+    @schedule = Schedule.new
+  end
+  def edit
+    @schedule = Schedule.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "schedules#index"
-  resources :schedules, only:[:index]
+  resources :schedules, only:[:index, :edit]
+  resources :friends, only:[:index, :new, :create]
 end
