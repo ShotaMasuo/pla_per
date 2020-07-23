ec25410a7173ad127f7cfab608dcbe20b39998a4
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-07-23 16:53:21 +0900

    Revert "Merge pull request #9 from ShotaMasuo/anotherWindowToCreateSchedule"
    This reverts commit c80fddd225d9ca2d742cbba812ef73817ea26e29, reversing
    changes made to 5dc8df1c1ccd51b8f9d1955eb63ef20da9cff4f1.

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -3,7 +3,7 @@ $(function(){
   function buildSchedule(schedule){
     let html = `
               <div class="task" style="grid-column: ${schedule.start_scale}/${schedule.finish_scale}; background-color: ${schedule.color};">
-                <span data-schedule_id="${schedule.id}">${schedule.name}</span>
+                <a href="/schedules/${schedule.id}/edit">${schedule.name}</a>
               </div>
               `
     return html;
@@ -27,13 +27,13 @@ $(function(){
       let width = width_records[schedule_id]
       let html = `
                   <div class="task" style="grid-column: ${width[0]}/${width[1]}; grid-row: 1/2; background-color: ${schedules[i]['color']};">
-                    <span data-schedule_id="${schedules[i].id}">${schedules[i]['name']}</span>
+                    <a href="/schedules/${schedule_id}/edit">${schedules[i]['name']}</a>
                   </div>
                 `
       if (width.length > 2){
         let html3 = `
                       <div class="task" style="grid-column: ${width[2]}/${width[3]}; grid-row: 3/4; background-color: ${schedules[i]['color']};">
-                      <span data-schedule_id="${schedules[i].id}">${schedules[i]['name']}</span>
+                        <a href="/schedules/${schedule_id}/edit">${schedules[i]['name']}</a>
                       </div>
                     `
         html += html3
@@ -65,7 +65,7 @@ $(function(){
       $('.add-schedule').prop('disabled', false);
     })
   });
-  
+
   $(document).on('click', '.day-icon', function(e){
     e.preventDefault();
     let day = $(this).attr("id");
