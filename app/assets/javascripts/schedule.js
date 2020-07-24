57b41b8d3298e019b901378fdc175f0dadfacf34
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-07-24 16:04:35 +0900

    CreateModalEditScreen

--- app/assets/javascripts/schedule.js
+++ app/assets/javascripts/schedule.js
@@ -3,7 +3,7 @@ $(function(){
   function buildSchedule(schedule){
     let html = `
               <div class="task" style="grid-column: ${schedule.start_scale}/${schedule.finish_scale}; background-color: ${schedule.color}; grid-row: 1/2; ">
-                <a href="/schedules/${schedule.id}/edit">${schedule.name}</a>
+                <span>${schedule.name}</span>
               </div>
               `
     return html;
@@ -26,14 +26,14 @@ $(function(){
       let schedule_id = schedules[i].id
       let width = width_records[schedule_id]
       let html = `
-                  <div class="task" style="grid-column: ${width[0]}/${width[1]}; grid-row: 1/2; background-color: ${schedules[i]['color']};">
-                    <a href="/schedules/${schedule_id}/edit">${schedules[i]['name']}</a>
+                  <div class="task" style="grid-column: ${width[0]}/${width[1]}; grid-row: 1/2; background-color: ${schedules[i]['color']}; data-schedule_id="${schedule_id}"">
+                    <span>${schedules[i]['name']}</span>
                   </div>
                 `
       if (width.length > 2){
         let html3 = `
-                      <div class="task" style="grid-column: ${width[2]}/${width[3]}; grid-row: 3/4; background-color: ${schedules[i]['color']};">
-                        <a href="/schedules/${schedule_id}/edit">${schedules[i]['name']}</a>
+                      <div class="task" style="grid-column: ${width[2]}/${width[3]}; grid-row: 3/4; background-color: ${schedules[i]['color']}; cursor: default;">
+                        <span>${schedules[i]['name']}</span>
                       </div>
                     `
         html += html3
@@ -42,6 +42,15 @@ $(function(){
     }
     return html2;
   }
+  function buildDone(s_scale, f_scale, color, name){
+    let addhtml = `
+                  <div class="task" style="grid-column: ${s_scale}/${f_scale}; grid-row: 3/4; background-color: ${color}; cursor: default;">
+                   ${name}
+                  </div>
+    `
+    return addhtml;
+  }
+
   $('.form-field__form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -65,7 +74,7 @@ $(function(){
       $('.add-schedule').prop('disabled', false);
     })
   });
-
+  
   $(document).on('click', '.day-icon', function(e){
     e.preventDefault();
     let day = $(this).attr("id");
@@ -92,8 +101,41 @@ $(function(){
       $(".under-leftbuttons").slideUp();
       $(".under-leftbuttons").removeClass('display-input');
     }else{
-    $(".under-leftbuttons").slideDown();
-    $(".under-leftbuttons").addClass('display-input');
-  }
+      $(".under-leftbuttons").slideDown();
+      $(".under-leftbuttons").addClass('display-input');
+    }
+  })
+  let doneurl = ""
+  $(document).on('click', '.task', function(){
+    doneurl = "/schedules/" + $(this).attr("data-schedule_id")
+    $('.edit-wrapper').fadeIn();
+    $('.edit-wrapper').addClass('to_block');
+  });
+  $(document).on('click', '.close-btn', function(){
+    $('.edit-wrapper').fadeOut();
+    $('.edit-wrapper').removeClass('to_block');
+  })
+  $('.edit-schedule__form').on('submit', function(e){
+    let formData = new FormData(this);
+    e.preventDefault();
+    $('.edit-wrapper').fadeOut();
+    $('.edit-wrapper').removeClass('to_block');
+    $.ajax({
+      url: doneurl,
+      type: "PATCH",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(schedule){
+      console.log(schedule.finished_scale);
+      $('.schedule-wrapper__plan').append(buildDone(schedule.started_scale, schedule.finished_scale, schedule.color, schedule.name));
+      $('.send-done').prop('disabled', false);
+    })
+    .fail(function(){
+      alert('更新できませんでした。')
+      $('.send-done').prop('disabled', false);
+    })
   })
 });
