b736e716816c9e209263517f88368269cc9ee1b2
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-07-30 11:45:52 +0900

    UpdateScheduleControllerToOrder

--- app/controllers/api/schedules_controller.rb
+++ app/controllers/api/schedules_controller.rb
@@ -1,5 +1,5 @@
 class Api::SchedulesController < ApplicationController
   def index
-    @schedules = Schedule.where(user_id: 1).where(sche_day: Date.today)
+    @schedules = Schedule.where(user_id: 1).where(sche_day: Date.today).order("start_time asc")
   end
 end
