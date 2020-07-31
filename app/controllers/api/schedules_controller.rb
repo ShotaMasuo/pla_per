a4f9921662eee3b8f8bbad9ee36100f2e79d96f2
ShotaMasuo <63441901+ShotaMasuo@users.noreply.github.com>
2020-07-31 12:50:35 +0900

    createingUpdateFunctionForAPI

--- app/controllers/api/schedules_controller.rb
+++ app/controllers/api/schedules_controller.rb
@@ -4,7 +4,21 @@ class Api::SchedulesController < ApplicationController
   end
   def searchPerson
     user = User.where(name: params[:id])
-    binding.pry
     @schedules = Schedule.where(user_id: user.ids).where(sche_day: Date.today).order("start_time asc")
+  end
+  def upSchedule
+    id = params[:id].to_i
+    get_started_time = params[:stime]
+    get_finished_time = params[:etime]
+    started_hour = get_started_time[0..1].to_i
+    started_minute = get_started_time[2..3].to_i
+    finished_hour = get_finished_time[0..1].to_i
+    finished_minute = get_finished_time[2..3].to_i
+    save_started_time = started_hour.to_s + ":" + started_minute.to_s
+    save_finished_time = finished_hour.to_s + ":" + finished_minute.to_s
+    schedule = Schedule.find(id)
+    schedule.started_time = save_started_time
+    schedule.finished_time = save_finished_time
+    schedule.save
   end
 end
